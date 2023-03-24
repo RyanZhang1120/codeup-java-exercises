516f8cf0c4e71469fc29426bb3ce820be9ab03ad
bcopy  ~/.ssh/id_rsa.pub <ryan.zhang1120@gmail.com>
2023-03-24 12:35:03 -0700

    syntax

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class HelloWorld {
+public class HelloWorld {
+    public static void main(String[] args) {
+        int myFavoriteNumber = 8;
+        System.out.println(myFavoriteNumber);
+        String myString = "3.14159";
+        System.out.println(myString);
+        float myNumber = 3.14f;
+        System.out.println(myNumber);
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
+        int y = 5;
+        System.out.println(++y);
+        System.out.println(y);
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) "three";
+//        System.out.println(three);
+        int z = 3;
+        int c = 4;
+        c = c * z;
+
+        System.out.println(c);
+
+
+    }
 }
