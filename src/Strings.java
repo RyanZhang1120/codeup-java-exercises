b4ca541280b95bed830e9e9a9a39dfa324b8dfac
bcopy  ~/.ssh/id_rsa.pub <ryan.zhang1120@gmail.com>
2023-03-27 13:58:28 -0700

    strings

--- src/Strings.java
+++ src/Strings.java
@@ -1,2 +1,14 @@
 public class Strings {
+    public static void main(String[] args) {
+        String education = "We don't need no education\n" +
+                "We don't need no thought control";
+        System.out.println(education);
+        String check = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(check);
+        String windows = "In windows, the main drive is usually C:\\";
+        System.out.println(windows);
+        String backslashes = "I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!";
+        System.out.println(backslashes);
+    }
 }
