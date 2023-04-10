0ec68872b361b5af238aae0ee404281c13944bef
bcopy  ~/.ssh/id_rsa.pub <ryan.zhang1120@gmail.com>
2023-04-09 21:45:40 -0700

    attendance

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -11,6 +11,8 @@ public class GradesApplication {
         Student student1 = new Student("Ryan");
         student1.addGrade(90);
         student1.addGrade(80);
+        student1.recordAttendance("2017-10-02", "A");
+        student1.recordAttendance("2017-10-03", "P");
 
         Student student2 = new Student("Niki");
         student2.addGrade(80);
@@ -34,24 +36,52 @@ public class GradesApplication {
 //            System.out.println(githubUsername + ": " + student.getName() +" - " + "Average grade: " + student.getGradeAverage());
 //        }
         System.out.println("Welcome!");
-        System.out.println("Here are the GitHub usernames of our students:");
-        for(String githubUsername : students.keySet()){
-            System.out.print("|" + githubUsername + "| ");
-        }
-        System.out.println("");
         boolean running = true;
         while(running){
-            System.out.println("What student would you like to see more information on?");
-            findStudent();
-            System.out.println("Would you like to see another student? y/n");
-            String goOn = scanner.nextLine();
-            if(goOn.equalsIgnoreCase("no") || goOn.equalsIgnoreCase("n")){
-                running = false;
-            } else if (goOn.equalsIgnoreCase("yes") || goOn.equalsIgnoreCase("y")) {
-                running = true;
-            } else {
-                System.out.println("Invalid input");
-                running = false;
+            String option;
+            boolean doLoop = true;
+            do{
+                System.out.println("Please choose the option No.\n 1. See all students' information\n 2. See an individual student's information\n 3. Exit");
+                option = scanner.nextLine();
+                switch (option) {
+                    case "1":
+                        for (String githubUsername : students.keySet()) {
+                            Student currentStudent = students.get(githubUsername);
+                            System.out.println(currentStudent.getName() + ", " + githubUsername + ", " + currentStudent.getGradeAverage());
+                        }
+                        doLoop = false;
+                        break;
+                    case "2":
+                        System.out.println("Here are the GitHub usernames of our students:");
+                        for(String githubUsername : students.keySet()){
+                            System.out.print("|" + githubUsername + "| ");
+                        }
+                        System.out.println("");
+                        System.out.println("Which student would you like to see more information on?");
+                        findStudent();
+                        doLoop = false;
+                        break;
+                    case "3":
+                        doLoop = false;
+                        running = false;
+                        break;
+                    default:
+                        System.out.println("Invalid option, please try again.");
+                        break;
+                }
+            }while(doLoop);
+
+            if(running){
+                System.out.println("Would you like to see another student? y/n");
+                String goOn = scanner.nextLine();
+                if(goOn.equalsIgnoreCase("no") || goOn.equalsIgnoreCase("n")){
+                    running = false;
+                } else if (goOn.equalsIgnoreCase("yes") || goOn.equalsIgnoreCase("y")) {
+                    running = true;
+                } else {
+                    System.out.println("Invalid input");
+                    running = false;
+                }
             }
         }
         System.out.println("Goodbye, and have a wonderful day!");
@@ -59,14 +89,15 @@ public class GradesApplication {
     public static void findStudent(){
         Scanner scanner = new Scanner(System.in);
         String userInput = scanner.nextLine();
+        Student student = students.get(userInput);
 
         if(students.containsKey(userInput)){
-            Student student = students.get(userInput);
             System.out.println("Name: " + student.getName() + " - GitHub Username: " + userInput );
             System.out.println("Current Average: " + student.getGradeAverage());
+            System.out.println("Attendance Rate: " + student.attendancePercentage() + "%");
+            System.out.println("Absent date(s): " + student.absentDays());
         } else {
             System.out.println("Sorry, no student found with the GitHub username of " + userInput);
         }
-
     }
 }
--- src/grades/Student.java
+++ src/grades/Student.java
@@ -1,12 +1,16 @@
 package grades;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class Student {
     private ArrayList<Integer> grades;
     private String name;
+    private HashMap<String, String> attendance;
     public  Student(String name) {
         this.name = name;
         this.grades = new ArrayList<>();
+        this.attendance = new HashMap<>();
     }
     // returns the student's name
     public String getName() {
@@ -30,4 +34,30 @@ public class Student {
         return sum / grades.size();
     };
 
+    public void recordAttendance(String date, String value) {
+        if(value.equalsIgnoreCase("A") || value.equalsIgnoreCase("P")){
+            attendance.put(date, value.toUpperCase());
+        } else {
+            System.out.println("Invalid value for attendance. Please use 'A' for Absent or 'P' for Present.");
+        }
+    }
+    public double attendancePercentage() {
+        double totalDays = attendance.size();
+        double absences = 0;
+        for (String status : attendance.values()) {
+            if (status.equalsIgnoreCase("A")) {
+                absences++;
+            }
+        }
+        return ((totalDays - absences) / totalDays) * 100;
+    }
+    public List<String> absentDays() {
+        List<String> absentDates = new ArrayList<>();
+        for (String date : attendance.keySet()) {
+            if (attendance.get(date).equalsIgnoreCase("A")) {
+                absentDates.add(date);
+            }
+        }
+        return absentDates;
+    }
 }
